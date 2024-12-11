d97ecc306bc16b69e965f84950cc85e4058b2819
tsukutan <mokipon.96.noir.22@gmail.com>
2024-12-12 02:10:37 +0900

    Add files via upload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,5 +1,5 @@
 let date = new Date();
 let year = date.getFullYear();
-let month = date.getMonth();
+let month = date.getMonth()+1;
 let day = date.getDate();
 console.log(year + '年' + month + '月' + day + '日');
