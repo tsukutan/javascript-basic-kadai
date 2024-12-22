37acb3f5906fd023953e4391333ce4bad7a4f600
tsukutan <mokipon.96.noir.22@gmail.com>
2024-12-22 23:42:17 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,6 @@
+const putBtn = document.getElementById('btn');
+const newText = document.getElementById('text');
+putBtn.addEventListener('click', () => {
+  newText.textContent = 'ボタンをクリックしました'
+});
+
